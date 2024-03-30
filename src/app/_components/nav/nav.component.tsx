5e1332281e63c31776b9aaffd068a4264fc574b3
LucasChanquia <lucaschanquia@gmail.com>
2024-03-29 21:17:57 -0300

    feat(shadcn): :art: componentes
    se agrega shadcn para trabajar con componentes

---
+++ src/app/_components/nav/nav.component.tsx
@@ -0,0 +1,34 @@
+import { list } from "@/utils/menuList";
+import Image from "next/image";
+import Link from "next/link";
+import "./nav.component.scss"
+
+export function NavComponent() {
+  return (
+    <>
+      <header>
+        <nav>
+          <div>
+            <Link href={"/"}>
+              <Image
+                src="/img/logo.png"
+                width={150}
+                height={50}
+                alt="FotoShop logo"
+              ></Image>
+            </Link>
+          </div>
+          <div>
+            <ul>
+              {list?.map(({ id, name, href }) => (
+                <Link href={href} key={id}>
+                  <li>{name}</li>
+                </Link>
+              ))}
+            </ul>
+          </div>
+        </nav>
+      </header>
+    </>
+  );
+}
---
+++ src/utils/menuList.ts
@@ -0,0 +1,17 @@
+export const list = [
+  {
+    id: 1,
+    name: "Inicio",
+    href: "/"
+  },
+  {
+    id: 2,
+    name: "Productos",
+    href: "products"
+  },
+  {
+    id: 3,
+    name: "Servicios",
+    href: "services"
+  },
+];
